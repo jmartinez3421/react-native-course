bc835f3bc3bb8c7122cf88a3d081e9d05d3912cb
Jordi Martínez <jmartinezg.fp@gmail.com>
2024-01-08 19:52:16 +0100

    FilmApp navigation configured

---
+++ FilmApp/src/navigation/StackNavigator.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen } from "../screens/HomeScreen.tsx";
+import { DetailScreen } from "../screens/DetailScreen.tsx";
+
+export type RootStackParams = {
+    Home: undefined;
+    Detail: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: "white",
+                },
+            }}
+        >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Detail" component={DetailScreen} />
+        </Stack.Navigator>
+    );
+};
