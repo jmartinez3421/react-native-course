46035adcb8a40ce7f80df2e570ccf26d44db63c5
Jordi Martínez <jmartinezg.fp@gmail.com>
2024-01-09 20:11:39 +0100

    Environment variables configured

--- FilmApp/src/screens/HomeScreen.tsx
+++ FilmApp/src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { API_URL } from "../api/environment.ts";
 
 export const HomeScreen = () => {
     return (
-        <View>
-            <Text>Hello</Text>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>{API_URL}</Text>
         </View>
     );
 };
