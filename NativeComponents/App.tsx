ddc3ed48e5ada1f54c002e583236efa204d70e9d
Jordi Martínez <jmartinezg.fp@gmail.com>
2024-01-24 19:50:54 +0100

    NativeComponents app started

---
+++ NativeComponents/App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
