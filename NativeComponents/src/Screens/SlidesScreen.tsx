bfe3db0e4d151802a0b17ad78a2aa9861c891438
Jordi Martínez <jmartinezg.fp@gmail.com>
2024-02-02 20:58:29 +0100

    Slider finished

--- NativeComponents/src/Screens/SlidesScreen.tsx
+++ NativeComponents/src/Screens/SlidesScreen.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View, Animated } from "react-native";
 import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
 import { Slide, SLIDER_DATA } from "../data/SliderData";
-
+import { StyledButton } from "../components/StyledButton";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { RootStackParams } from "../navigation/StackNavigator";
 const Pagination = ({
     index,
     numberOfData,
@@ -12,21 +14,47 @@ const Pagination = ({
     numberOfData: number;
     onClick: (index: number) => void;
 }) => {
+    const { fadeIn, fadeOut, opacity } = useAnimation();
+
+    const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+    const showButton = React.useMemo(() => index === numberOfData - 1, [index, numberOfData]);
+
+    React.useEffect(() => {
+        if (showButton) {
+            fadeIn();
+        } else {
+            fadeOut();
+        }
+    }, [showButton]);
+
     return (
-        <View style={styles.pagination}>
-            {Array.from(Array(numberOfData).keys()).map((_, i) => (
-                <TouchableOpacity key={i} onPress={() => onClick(i)}>
-                    <View
-                        style={{
-                            ...styles.dot,
-                            backgroundColor: i === index ? "purple" : "gray",
-                        }}
-                    />
-                </TouchableOpacity>
-            ))}
+        <View style={styles.paginationContainer}>
+            <View style={styles.pagination}>
+                {Array.from(Array(numberOfData).keys()).map((_, i) => (
+                    <TouchableOpacity key={i} onPress={() => onClick(i)}>
+                        <View
+                            style={{
+                                ...styles.dot,
+                                backgroundColor: i === index ? "purple" : "gray",
+                            }}
+                        />
+                    </TouchableOpacity>
+                ))}
+            </View>
+            <Animated.View style={{ opacity }}>
+                <StyledButton
+                    onPress={() => navigation.navigate("HomeScreen")}
+                    label="Start"
+                    icon="chevron-forward-outline"
+                    disabled={!showButton}
+                />
+            </Animated.View>
         </View>
     );
 };
+
+import { useAnimation } from "../hooks/useAnimation";
 
 const SlideItem = ({ item }: { item: Slide }) => {
     return (
@@ -51,7 +79,7 @@ export const SlidesScreen = () => {
 
     const width = Dimensions.get("window").width;
     return (
-        <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
             <Carousel
                 ref={carouselRef}
                 loop={false}
@@ -61,7 +89,7 @@ export const SlidesScreen = () => {
                 onSnapToItem={(index) => setActiveIndex(index)}
             />
             <Pagination index={activeIndex} numberOfData={SLIDER_DATA.length} onClick={handlePaginationClick} />
-        </SafeAreaView>
+        </View>
     );
 };
 
@@ -69,10 +97,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginHorizontal: -10,
-        paddingVertical: 20,
+        justifyContent: "center",
+        paddingVertical: 50,
     },
     slideContainer: {
-        flex: 1,
         justifyContent: "center",
         padding: 40,
     },
@@ -92,13 +120,19 @@ const styles = StyleSheet.create({
         fontWeight: "normal",
         marginBottom: 20,
     },
+    paginationContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 40,
+    },
     pagination: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
         columnGap: 15,
-        paddingBottom: 20,
     },
     dot: {
         width: 15,
--- NativeComponents/src/components/StyledButton.tsx
+++ NativeComponents/src/components/StyledButton.tsx
@@ -1,17 +1,24 @@
-import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet, StyleProp, ViewStyle } from "react-native";
 import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 type Props = {
     onPress: () => void;
     label: string;
+    icon?: keyof typeof Ionicons.glyphMap;
+    containerStyle?: StyleProp<ViewStyle>;
+    disabled?: boolean;
 };
 
-export const StyledButton = ({ onPress, label }: Props) => (
-    <TouchableOpacity onPress={onPress}>
-        <View style={styles.button}>
-            <Text style={styles.text}>{label}</Text>
-        </View>
-    </TouchableOpacity>
+export const StyledButton = ({ onPress, label, icon, containerStyle, disabled }: Props) => (
+    <View style={containerStyle}>
+        <TouchableOpacity onPress={onPress} disabled={disabled}>
+            <View style={styles.button}>
+                <Text style={styles.text}>{label}</Text>
+                {icon && <Ionicons name={icon} size={24} color="white" />}
+            </View>
+        </TouchableOpacity>
+    </View>
 );
 
 const styles = StyleSheet.create({
@@ -19,6 +26,11 @@ const styles = StyleSheet.create({
         backgroundColor: "purple",
         borderRadius: 5,
         padding: 10,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        columnGap: 10,
     },
     text: {
         color: "white",
